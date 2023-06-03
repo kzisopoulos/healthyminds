8d95b65b8e59a9441ff0bfa4afa52375e802d946
kzisopoulos <k91.zis@gmail.com>
2023-06-03 16:27:40 +0300

    Added under construction on services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,9 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
 import { FC } from "react";
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <div>Services Page</div>;
+  return (
+    <section className="py-8">
+      <h1 className="heading_text mb-5 max-w-4xl">
+        Υπό κατασκευή. Το τμήμα των{" "}
+        <span className="orange_gradient">υπηρεσιών</span> του site θα είναι
+        σύντομα διαθέσιμο!
+      </h1>
+      <Image
+        src={"./under_construction.svg"}
+        alt={"Εικόνα που δείχνει εργάτες να κατασκευάζουν ένα σπίτι."}
+        width={300}
+        height={300}
+        className="mx-auto my-10"
+      />
+      <Link
+        href="/"
+        className="inline-block uppercase orange_background text-white font-semibold py-2 px-4 rounded-md"
+      >
+        Επιστροφη στην αρχικη
+      </Link>
+    </section>
+  );
 };
 
 export default page;
